901cf5c471edc8871bd952c7a2981e4f0a9c66e0
Gareth Paterson <garethapaterson@gmail.com>
2021-12-31 14:01:25 -0700

    Add download endpoint and refactor to allow for streamlined downloading

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import (
-    Flask, Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Flask, Blueprint, flash, g, redirect, render_template, request, session, url_for, make_response
 )
 import pandas
 
@@ -32,8 +32,7 @@ def highlight_touchdown_rush(x):
     return style_dataframe
 
 
-def get_rushing_html(player_name=None, category_sort=None):
-    ''' Filter and style the dataframe to render in the expected format '''
+def filter_rushing_data(player_name=None, category_sort=None):
     rushing_data = get_rushing_dataframe()
 
     if player_name:
@@ -42,6 +41,13 @@ def get_rushing_html(player_name=None, category_sort=None):
     if category_sort:
         category_sort = category_sort.split(' - ')
         rushing_data = rushing_data.sort_values(by=[category_sort[0]], ascending=[category_sort[1] == 'ASC'])
+
+    return rushing_data
+
+
+def get_rushing_html(player_name=None, category_sort=None):
+    ''' Style the dataframe to render in the expected format '''
+    rushing_data = filter_rushing_data(player_name, category_sort)
 
     rushing_data = rushing_data.style.format(
         precision=1, thousands=''
@@ -56,12 +62,25 @@ def rushing_table():
     return render_template('index.html', data=rushing_data)
 
 
-@app.route('/filter_data', methods=['POST'])
+@app.route('/filter_data', methods=['GET'])
 def filter_data():
-    player_name = request.form.get('player_name')
-    category_sort = request.form.get('category_sort')
+    player_name = request.args.get('player_name')
+    category_sort = request.args.get('category_sort')
 
     return get_rushing_html(player_name, category_sort)
+
+
+@app.route('/download_data', methods=['GET'])
+def download_data():
+    player_name = request.args.get('player_name')
+    category_sort = request.args.get('category_sort')
+
+    rushing_data = filter_rushing_data(player_name, category_sort)
+
+    response = make_response(rushing_data.to_csv(index=False))
+    response.headers["Content-Disposition"] = "attachment;"
+    response.headers["Content-Type"] = "text/csv"
+    return response
 
 
 if __name__ == "__main__":
--- static/rushing.js
+++ static/rushing.js
@@ -1,19 +1,27 @@
 document.querySelector("#table-filter").addEventListener("submit", (event) => {
-	event.preventDefault();
+    event.preventDefault();
 
-	const XHR = new XMLHttpRequest();
+    const XHR = new XMLHttpRequest();
     const FD = new FormData(event.target);
+    const paramString = new URLSearchParams(FD).toString();
+    const action = event.submitter.value
+    const uri = `${action}_data?${paramString}`
+    const url = `${window.origin}/${uri}`
 
     XHR.onreadystatechange = request => {
-    	const response = request.target;
-    	
-    	if (response.readyState === 4 && response.status === 200) {
-    		document.querySelector('#table-data').innerHTML = response.responseText
-    	}
+        const response = request.target;
+
+        if (response.readyState === 4 && response.status === 200) {
+            if (action === 'filter') {
+                document.querySelector('#table-data').innerHTML = response.responseText
+            } else if (action === 'download') {
+                let link = document.querySelector('#download_link')
+                link.href = url
+                link.click()
+            }
+        }
     };
 
-    XHR.open( "POST", `${window.origin}/filter_data`);
+    XHR.open("GET", url);
     XHR.send(FD)
-
-    console.log('meow');
 });
